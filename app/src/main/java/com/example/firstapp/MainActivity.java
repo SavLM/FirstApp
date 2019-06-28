f20f7aa6eeff0c0a66813efae7b637dad3b77c8b
SavLM <50849597+SavLM@users.noreply.github.com>
2019-06-27 20:39:07 -0400

    finished text onclick app

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSubmit;
+        final EditText txtInput;
+        final TextView txtDisplay;
+
+        btnSubmit = findViewById(R.id.main_button_view);
+        txtInput = findViewById(R.id.input_text_view);
+        txtDisplay = findViewById(R.id.output_text_view);
+
+        // define onclick method of btnSubmit
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtDisplay.setText(txtInput.getText());
+            }
+        });
     }
 }
